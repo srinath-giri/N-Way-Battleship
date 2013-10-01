61a6f52222a93bff6072a389b17eab6ba7ec4de4
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-09-30 19:04:02 -0700

    Adding in initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoR007::Application.config.secret_token = '78980de51074a7522ef10a02980f38d3f0560e41b7e3baf619525a97fcf983592c5bb1de1b6267696d28f904e37369516b84a009bd20033568c86dec4a52990b'
