284561c98d785e377c58dd864f0580237ece8f33
Maria Luisa Carrion D <marilu597@gmail.com>
2013-10-12 02:31:32 -0400

    Delete blank lines to make functions more compact

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,16 +1,12 @@
 class PlayersController < ApplicationController
 
   def self.pass_turn(players)
-
     current_player = find_player_with_token(players)
     next_player = determine_next_player(players, current_player)
-
     if current_player # if it's not nil
       current_player.turn = false
     end
-
     next_player.turn = true
-
   end
 
   def self.find_player_with_token(players)
@@ -23,12 +19,9 @@ class PlayersController < ApplicationController
   end
 
   def self.determine_next_player(players, current_player)
-
-
     # we determine the next player based on the player id
     players.sort_by! { |player| player[:id] }
     # just modify the previous line if you want to assign turns based on something different than the player id
-
     if current_player
       current_player_index = players.index(current_player)
       if current_player_index < players.size - 1
@@ -39,7 +32,6 @@ class PlayersController < ApplicationController
     else
       return players[0]  # return first player if none has the token
     end
-
   end
 
 end
