98b2219b0467f8897b733094599ca2c43f9c7436
Maria Luisa Carrion D <marilu597@gmail.com>
2013-10-12 00:25:08 -0400

    Add test in players_controller_spec.rb

--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 
 describe PlayersController do
 
+  it "passes the token to the player with the first ID if no player has the token" do
+    player1 = Player.create(name: "Grace", turn: false)
+    player2 = Player.create(name: "Ibrahim", turn: false)
+    player3 = Player.create(name: "Owen", turn: false)
+    player4 = Player.create(name: "Srinath", turn: false)
 
+    PlayersController.passTurn
+
+    player1.turn.should be_true
+  end
 
 end
