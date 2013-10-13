53cf5e3620daba66044677492d80bcd92eba13c8
Srinath Giri <srinathcbe43@gmail.com>
2013-10-13 17:38:17 -0400

    Validating presence of name and grid points for Ship Model

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,3 +1,8 @@
 class Ship < ActiveRecord::Base
   attr_accessible :name, :state, :x_end, :x_start, :y_end, :y_start
+  validates_presence_of(:name)
+  validates_presence_of(:x_start)
+  validates_presence_of(:x_end)
+  validates_presence_of(:y_start)
+  validates_presence_of(:y_end)
 end
--- spec/models/ship_spec.rb
+++ spec/models/ship_spec.rb
@@ -2,19 +2,41 @@ require 'spec_helper'
 
 describe Ship do
 
-  before do
-    @ship = Ship.new(name:"Destroyer", x_start:1, x_end:1, y_start:2, y_end:2, state:nil) 
-     
+  context "check attribute access" do
+
+    before do
+      @ship = Ship.new(name: "Destroyer", x_start: 1, x_end: 1, y_start: 1, y_end: 4, state: nil)
+    end
+
+    subject { @ship }
+
+    it { should respond_to(:name) }
+    it { should respond_to(:x_start) }
+    it { should respond_to(:x_end) }
+    it { should respond_to(:y_start) }
+    it { should respond_to(:y_end) }
+    it { should respond_to(:state) }
 
   end
 
-  subject {@ship}
+  context "arrange ships" do
 
-  it {should respond_to(:name)}
-  it {should respond_to(:x_start)}
-  it {should respond_to(:x_end)}
-  it {should respond_to(:y_start)}
-  it {should respond_to(:y_end)}
-  it {should respond_to(:state)}
+    it "raises error if saved without name" do
+      ship = Ship.new
+      expect { ship.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it "raises error if saved without grid points" do
+      ship = Ship.new(name: "Destroyer")
+      expect { ship.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      ship = Ship.new(name: "Destroyer", x_start: 1)
+      expect { ship.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      ship = Ship.new(name: "Destroyer", x_start: 1, x_end: 1)
+      expect { ship.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      ship = Ship.new(name: "Destroyer", x_start: 1, x_end: 1, y_start: 1)
+      expect { ship.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+  end
 
 end
