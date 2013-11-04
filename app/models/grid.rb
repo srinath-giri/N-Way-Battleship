a79744c19f9f199249da7f32f9c1233be619a6ae
Srinath Giri <srinathcbe43@gmail.com>
2013-11-04 17:30:07 -0500

    Grid Validations

--- app/models/grid.rb
+++ app/models/grid.rb
@@ -2,6 +2,8 @@ class Grid < ActiveRecord::Base
   attr_accessible :player_id, :grid_type
 
   belongs_to :player
-  has_many :cells    
-  
+  has_many :cells
+
+  validates_inclusion_of :grid_type, :in => %w( battlefield my_ships )
+
 end
--- spec/models/grid_spec.rb
+++ spec/models/grid_spec.rb
@@ -5,13 +5,25 @@ describe Grid do
     @grid = Grid.new(player_id: 1, grid_type: "battlefield")
   end
   
-  
   context "attribute access" do
     subject { @grid }
     it { should respond_to(:player_id) }
     it { should respond_to(:grid_type) }
-    
   end
+
+  context "grid validations" do
+
+    it "Grid type is either battlefield or my_ships" do
+      grid = Grid.new(player_id: 1, grid_type: "bf")
+      expect { grid.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      grid = Grid.new(player_id: 1, grid_type: "battlefield")
+      expect { grid.save! }.to change { Grid.count }.by(1)
+      grid = Grid.new(player_id: 1, grid_type: "my_ships")
+      expect { grid.save! }.to change { Grid.count }.by(1)
+    end
+
+  end
+
 end
   
 
