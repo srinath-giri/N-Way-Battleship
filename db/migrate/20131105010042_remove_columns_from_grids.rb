bde0e2b8d35bd0689256275d89e2141082d35cd0
Maria Luisa Carrion D <marilu597@gmail.com>
2013-11-12 15:41:21 -0500

    Remove faulty migrations

--- db/migrate/20131105010042_remove_columns_from_grids.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColumnsFromGrids < ActiveRecord::Migration
-  def up
-    remove_column :grids, :columns
-  end
-
-  def down
-    add_column :grids, :columns, :integer
-  end
-end
--- db/migrate/20131105010111_remove_rows_from_grids.rb
+++
@@ -1,9 +0,0 @@
-class RemoveRowsFromGrids < ActiveRecord::Migration
-  def up
-    remove_column :grids, :rows
-  end
-
-  def down
-    add_column :grids, :rows, :integer
-  end
-end
